2824cb472fd715a32867c35b16661b4ba99ef06f
wolfganghuse <wolfgang.huse@nutanix.com>
2022-02-18 16:43:05 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,6 @@ oc adm policy add-cluster-role-to-user cluster-monitoring-view -z grafana-servic
 export BEARER_TOKEN=$(oc serviceaccounts get-token grafana-serviceaccount -n grafana)
 export esPassword=$(oc get secret -n elastic elasticsearch-es-elastic-user  -o go-template='{{.data.elastic | base64decode}}')
 export esRoute=$(oc get route  -n elastic elasticsearch --no-headers -o custom-columns=NAME:.spec.host)
-export promRoute=$(shell oc get route  -n openshift-monitoring prometheus-k8s --no-headers -o custom-columns=NAME:.spec.host) 
 
 envsubst < environment/prometheus-grafanadatasource.yaml | oc create -f -
 envsubst < environment/elastic-grafanadatasource.yaml | oc create -f -
@@ -31,6 +30,8 @@ oc create sa benchmark
 oc adm policy add-cluster-role-to-user cluster-monitoring-view -z benchmark -n benchmark
 exit
 
-promToken=$(shell oc serviceaccounts get-token benchmark -n benchmark)
+export promToken=$(shell oc serviceaccounts get-token benchmark -n benchmark)
+export promRoute=$(shell oc get route  -n openshift-monitoring prometheus-k8s --no-headers -o custom-columns=NAME:.spec.host) 
+
 cd workload/$(WORKLOAD)
 kube-burner init -c workload.yml -u https://${promRoute} -t ${promToken} --step=30s -m metrics.yaml --uuid=${UUID} --log-level=info
