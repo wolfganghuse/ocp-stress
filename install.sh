748b489a2ed1a9dc92d2f57ba5201d67c9866fe5
wolfganghuse <wolfgang.huse@nutanix.com>
2022-02-24 17:23:39 +0100

    install

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/cloud-bulldozer/kube-burner/releases/download/v0.15.1/kube-burner-0.15.1-Linux-x86_64.tar.gz
+tar xzvf kube-burner-0.15.1-Linux-x86_64.tar.gz
+sudo install kube-burner /usr/bin
