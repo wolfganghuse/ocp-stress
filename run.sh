eb6bdeaed1fa88d2ffb4eb11857b41ef88c132d6
wolfganghuse <wolfgang.huse@nutanix.com>
2022-10-01 11:09:18 +0200

    Update run.sh
    added esServer for e2e

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ export UUID=$(uuidgen)
 export esPassword=$(oc get secret -n elastic elasticsearch-es-elastic-user  -o go-template='{{.data.elastic | base64decode}}')
 export esRoute=$(oc get route  -n elastic elasticsearch --no-headers -o custom-columns=NAME:.spec.host)
 
+export esServer=https://elastic:$esPassword@$esRoute
+
 export promRoute=$(oc get route  -n openshift-monitoring prometheus-k8s --no-headers -o custom-columns=NAME:.spec.host)
 export promToken=$(oc serviceaccounts get-token benchmark -n benchmark)
 cd workload/${WORKLOAD}
